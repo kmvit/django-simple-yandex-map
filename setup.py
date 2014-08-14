5511976b25b21a71fd072fd9e9a74b7f9a6af6b8
Max Vyaznikov <maxvyaznikov@gmail.com>
2014-08-14 20:32:58 +0400

    version bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_ymap',
-    version='1.1',
+    version='1.2',
 
     description='Simple Yandex.Maps integration into admin panel',
     keywords='django, yandex, maps, admin',
